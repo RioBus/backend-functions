45fdbdb85fa20ea0328f2515d2b0433ccd14475e
Fred Souza <fredericoamsouza@gmail.com>
2019-06-24 22:15:36 -0300

    Adding the scheduled crawler on the play

--- functions/src/crawler.js
+++ functions/src/crawler.js
@@ -1,9 +1,3 @@
-const functions = require('firebase-functions');
-//import * as admin from 'firebase-admin';
-//import { getInstance } from './firebase';
-
-//const client: admin.app.App = getInstance();
-
-export const crawler = functions.pubsub.schedule('3 seconds').onRun(context => {
+module.exports = (context) => {
     console.log('Executing...');
-});
+};
--- functions/src/index.js
+++ functions/src/index.js
@@ -1,5 +1,8 @@
 const functions = require('firebase-functions');
 
 const api = require('./api');
+const crawler = require('./crawler');
 
 exports.v3 = functions.https.onRequest(api);
+exports.crawler = functions.pubsub.schedule('every 3 seconds').onRun(crawler);
+
